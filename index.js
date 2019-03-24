595d2fbbbcaeffd025be7774464f131cbc0a4835
Dmitrii Prone <idimzz@gmail.com>
2019-03-24 20:38:26 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const dataSource = require('./chart_data.json');
+
+http.createServer((req, res) => {
+  let code;
+  const urlParsed = url.parse(req.url);
+  let resData;
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+	if (req.method === 'OPTIONS') {
+		res.writeHead(200);
+		res.end();
+		return;
+  }
+
+  if (urlParsed.pathname === '/chart_data.json') {
+    code = 200;
+    resData = {
+      code,
+      message: 'success',
+      data: dataSource[querystring.parse(urlParsed.query).n],
+    };
+  } else {
+    code = 404;
+    resData = {
+      code,
+      message: 'not found',
+    };
+  }
+
+  res.writeHead(code, { 'Content-Type': 'text/json' });
+  res.end(JSON.stringify(resData));
+}).listen(1234);
